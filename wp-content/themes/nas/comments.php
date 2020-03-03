6142e9d3bc44a091dae1c4a3a8f45fb9e940307f
kosars <kosarsMK@gmail.com>
2020-03-03 18:45:40 +0200

    commens-test

---
+++ wp-content/themes/nas/comments.php
@@ -0,0 +1,27 @@
+<?php
+if ( post_password_required() ) {
+	return;
+}
+?>
+<section id="comments" class="comments wrapper comments-area">
+	<?php if ( have_comments() ) :?>
+	<?php the_comments_navigation(); ?>
+    <h1>КОММЕНТАРИИ</h1>
+    <div class="comments-container">
+        <!-- <div class="comment">
+            <p class="comment-name">Анастасия Саламаха</p>
+            <p class="comment-text">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            <p class="comment-date">9 апреля 1228 в 22:28</p>
+        </div> -->
+	</div>
+	<?endif; // Check for have_comments().
+
+	comment_form();
+	?>
+    <!-- <form class="comment-form-container">
+        <input type="text" name="name" placeholder="Имя">
+        <input type="email" name="email" placeholder="Электронная почта">
+        <textarea name="message" rows="3" placeholder="Комментарий"></textarea>
+        <input type="submit" value="ДОБАВИТЬ КОММЕНТАРИЙ" class="button">
+    </form> -->
+</section>
--- wp-content/themes/nas/functions.php
+++ wp-content/themes/nas/functions.php
@@ -117,5 +117,29 @@ if( function_exists('add_image_size')){
 	add_image_size( 'family-news-prewiev', 414, 322, true);
 	add_image_size( 'calendar-prewiev', 162, 162, true);
 }
+## Исполняемый PHP код в контенте записи WordPress.
+## [exec]PHP_код[/exec]
+##
+## @version: 1.0
+if( 'Исполняемый PHP код в контенте' ){
 
+	add_filter( 'the_content', 'content_exec_php', 0 );
+
+	function content_exec_php( $content ){
+		return preg_replace_callback( '/\[exec( off)?\](.+?)\[\/exec\]/s', '_content_exec_php', $content );
+	}
+
+	function _content_exec_php( $matches ){
+
+		if( ' off' === $matches[1] ){
+			return "\n\n<".'pre>'. htmlspecialchars( $matches[2] ) .'</pre'.">\n\n";
+		}
+		else {
+			eval( "ob_start(); {$matches[2]}; \$exec_php_out = ob_get_clean();" );
+			return $exec_php_out;
+		}
+
+	}
+
+}
 ?>
